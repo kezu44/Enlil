498a5b6f5f443d13680f580991244f174c5561c8
kezu44 <165667691+kezu44@users.noreply.github.com>
2024-05-18 14:09:00 +0200

    Js add search input

--- src/script.js
+++ src/script.js
@@ -1,1 +1,7 @@
 //standart info
+
+function handleSearchSubmit(event) {
+    event.preventDefault()
+}
+let searchformElement= document.querySelector("search-form");
+searchformElement.addEventListener("submit", handleSearchSubmit);
