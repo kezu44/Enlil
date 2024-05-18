20614b62b0d1df33924c34674c59e296732220c2
kezu44 <165667691+kezu44@users.noreply.github.com>
2024-05-18 14:12:23 +0200

    JS  adding replacing h1

--- src/script.js
+++ src/script.js
@@ -2,6 +2,10 @@
 
 function handleSearchSubmit(event) {
     event.preventDefault()
+    let searchInput = document.querySelector("#search-input");
+    let citynameElement = document.querySelector("#cityname");
+    citynameElement.innerHTML = searchInput.ariaValueMax;
+
 }
 let searchformElement= document.querySelector("search-form");
 searchformElement.addEventListener("submit", handleSearchSubmit);
