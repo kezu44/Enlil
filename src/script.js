f8f0fe756af665e169c34a766cc925c9e4452e13
kezu44 <165667691+kezu44@users.noreply.github.com>
2024-05-19 11:41:47 +0200

    adding search API
    adding axios API
    changing the Unit UI

--- src/script.js
+++ src/script.js
@@ -1,11 +1,25 @@
 //standart info
+function refreshWeather(response) {
+   let temperatureElement = document.querySelector("#tempdisplay");
+   let temperature = response.data.temperature.current;
+   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = "ac0e90fab3b874b0te420f3c3fe852oa";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+   axios.get(apiUrl).then(refreshWeather);
+   
+}
 
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
     let citynameElement = document.querySelector("#cityname");
     citynameElement.innerHTML = searchInput.value;
-
+    searchCity(searchInput.value);
 }
+
+
 let searchformElement= document.querySelector("#search-form");
 searchformElement.addEventListener("submit", handleSearchSubmit);
